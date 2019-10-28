aa16eff7ef23e432a18e1f136d621920f96c2732
Benedita Tanabi <beneditatan@Beneditas-MacBook-Pro.local>
2019-10-28 17:27:13 +0800

    added checker functions to HTMLChecker

--- src/logic/HTMLChecker.js
+++ src/logic/HTMLChecker.js
@@ -1,9 +1,42 @@
-class HTMLChecker 
-{   
-	constructor(htmlObj) {
-		this.htmlObj = htmlObj;
+const checkTagCompleteness = (node, attr, attrVal = '') => {
+	let valid = false;
+	var attrObj = node.attributes;
+
+	if (attr in attrObj)
+	{
+		if (attrVal === '')
+		{
+			valid = true;
+		}
+		else 
+		{
+			valid = attrVal == attrObj[attr] ? true : false;
+		}
+	} 
+	else 
+	{
+		valid = false;
 	}
-    
+
+	return valid;
 }
 
-module.exports = HTMLChecker;
+const checkTagExist = (root, tag) => {
+	const nodes = root.querySelectorAll(tag);
+
+	return nodes
+
+}
+
+const checkNumberOfTag = (root, tag) => {
+	const nodes = root.querySelectorAll(tag);
+
+	return nodes.length;
+
+}
+
+module.exports = {
+	checkNumberOfTag,
+	checkTagCompleteness,
+	checkTagExist
+};
